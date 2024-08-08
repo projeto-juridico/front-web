e756304790076a78a428ec9c10bb6c8cd5e24330
andretosin <andre_tosin@hotmail.com>
2024-08-08 10:31:53 -0300

    testing auto deploy

--- front-web/src/App.js
+++ front-web/src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello, World!</h1>
+        <h1>Testing auto deploy</h1>
       </header>
     </div>
   );
