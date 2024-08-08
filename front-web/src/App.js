1ecc5e30d363e1b816aa982960c6754391d62908
andretosin <andre_tosin@hotmail.com>
2024-08-08 10:47:50 -0300

    testing branch protection

--- front-web/src/App.js
+++ front-web/src/App.js
@@ -6,7 +6,7 @@ function App() {
       <header className="App-header">
         <h1>Hello, World!</h1>
         <h1>Testing auto deploy</h1>
-        <h1>Testing branch protection</h1>
+        <h1>Testing branch protection 2</h1>
       </header>
     </div>
   );
